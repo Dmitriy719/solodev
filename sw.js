26b0e7c6ac06a9f431f0b2b9d9bbe3e4ddad3610
Dmitriy719 <88295651+Dmitriy719@users.noreply.github.com>
2026-08-07 19:23:54 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,29 +1,42 @@
-const CACHE = 'solodev-v1';
-const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
+const VERSION = 'v0.8-' + Date.now();
+const CACHE_NAME = 'solodev-' + VERSION;
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon.svg'
+];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(ks => Promise.all(
-    ks.filter(k => k !== CACHE).map(k => caches.delete(k))
-  )));
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
-  if (e.request.method !== 'GET') return;
+  if (e.request.url.endsWith('.html') || e.request.url.endsWith('/')) {
+    e.respondWith(
+      fetch(e.request, { cache: 'no-store' })
+        .then(r => {
+          const copy = r.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(e.request, copy));
+          return r;
+        })
+        .catch(() => caches.match(e.request))
+    );
+    return;
+  }
   e.respondWith(
-    caches.match(e.request, { ignoreSearch: true }).then(r =>
-      r || fetch(e.request).then(res => {
-        if (res.ok && new URL(e.request.url).origin === location.origin) {
-          const cp = res.clone();
-          caches.open(CACHE).then(c => c.put(e.request, cp));
-        }
-        return res;
-      }).catch(() => caches.match('./index.html'))
-    )
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
