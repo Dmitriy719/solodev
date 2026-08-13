5371c446e924520b78e5a993f35fa8c51f70dc10
Dmitriy719 <Demon.osipov2011@yandex.ru>
2026-08-13 00:17:02 +0000

    fix: add cache reset button and fix sw.js caching

--- sw.js
+++ sw.js
@@ -1,23 +1,22 @@
-const CACHE = 'solodev-v081';
+const CACHE_NAME = 'solodev-v2';
+const urlsToCache = ['/solodev/'];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll([
-    './', './index.html', './manifest.webmanifest', './icon.svg'
-  ]).catch(err => console.log(err))));
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
   self.skipWaiting();
 });
 
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => 
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
-  self.clients.claim();
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
 });
 
-self.addEventListener('fetch', e => {
-  if (e.request.method !== 'GET') return;
-  
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(names => Promise.all(names.filter(n => n !== CACHE_NAME).map(n => caches.delete(n))))
   );
+  self.clients.claim();
 });
