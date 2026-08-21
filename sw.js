1b9da23b929ff6bea3a0c335e782bcace1552070
Dmitriy719 <Demon.osipov2011@yandex.ru>
2026-08-21 07:03:54 +0000

    v6.33.3: update Service Worker cache to v6.33.3 and add smart_assistant.js

--- sw.js
+++ sw.js
@@ -1,22 +1,50 @@
-const CACHE_NAME = 'solodev-v2';
-const urlsToCache = ['/solodev/'];
+var CACHE_NAME = 'solodev-v6.33.3';
+var urlsToCache = [
+  '/',
+  '/solodev/',
+  '/index.html',
+  '/app.min.js',
+  '/smart_assistant.js',
+  '/style.css',
+  '/manifest.webmanifest'
+];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Cache opened:', CACHE_NAME);
+        return cache.addAll(urlsToCache);
+      })
   );
   self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', function(event) {
   event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
   );
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener('activate', function(event) {
+  var cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
-    caches.keys().then(names => Promise.all(names.filter(n => n !== CACHE_NAME).map(n => caches.delete(n))))
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
   self.clients.claim();
 });
